04e7ec0ad2b53ef471cde1c943b55f6a8637d482
Abuenameh Aiyejina <abuenameh@gmail.com>
2014-10-20 20:35:51 -0400

    Commit

--- BoseHubbardStatics.py
+++ BoseHubbardStatics.py
@@ -3,9 +3,56 @@ import time
 import sys
 import MPSPyLib as mps
 import numpy as np
+import concurrent.futures
+import subprocess
+import gtk
+import gobject
 import matplotlib.pyplot as plt
+from mathematica import mathformat
 from matplotlib import cm
+from speed import gprogress
 
+numthreads = 2
+
+def execute(command):
+    """ Execute the given command on the command line."""
+    # print subprocess.list2cmdline(command)
+    return subprocess.call(subprocess.list2cmdline(command), shell=True)
+
+def runmps(infile):
+    appname='Execute_MPSMain'
+    cmdline = [appname]
+    cmdline += [infile]
+    execute(cmdline)
+
+
+def run(MainFiles):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=numthreads) as executor:
+        futures = [executor.submit(runmps, infile) for infile in MainFiles]
+        for future in gprogress(concurrent.futures.as_completed(futures), size=len(futures)):
+            pass
+    gtk.main_quit()
+
+
+N = 1000;
+g13 = 2.5e9;
+g24 = 2.5e9;
+Delta = -2.0e10;
+alpha = 1.1e7;
+
+Ng = np.sqrt(N) * g13;
+
+def JW(W):
+    lenW = len(W)
+    J = np.zeros(lenW)
+    for i in range(0, lenW-1):
+        J[i] = alpha * W[i] * W[i+1] / (np.sqrt(Ng * Ng + W[i] * W[i]) * np.sqrt(Ng * Ng + W[i+1] * W[i+1]))
+    J[lenW-1] = alpha * W[lenW-1] * W[0] / (np.sqrt(Ng * Ng + W[lenW-1] * W[lenW-1]) * np.sqrt(Ng * Ng + W[0] * W[0]))
+    return J
+
+
+def UW(W):
+    return -2*(g24 * g24) / Delta * (Ng * Ng * W * W) / ((Ng * Ng + W * W) * (Ng * Ng + W * W))
 
 t0 = time.time()
 
@@ -23,14 +70,14 @@ def plotIt(jvalues, muvalues, dependentvalues):
     plt.show()
 
 
-PostProcess = int(sys.argv[1])#False if sys.argv[1] == 0 else True
+PostProcess = int(sys.argv[1])
 
 # Build operators
 Operators = mps.BuildBoseOperators(6)
 Operators['interaction'] = 0.5 * (np.dot(Operators['nbtotal'], Operators['nbtotal']) - Operators['nbtotal'])
 #Define Hamiltonian MPO
 H = mps.MPO()
-H.AddMPOTerm(Operators, 'bond', ['bdagger', 'b'], hparam='t', weight=-1.0)
+H.AddMPOTerm(Operators, 'bond', ['bdagger', 'b'], hparam='J', weight=-1.0)
 H.AddMPOTerm(Operators, 'site', 'interaction', hparam='U', weight=1.0)
 
 #ground state observables
@@ -43,13 +90,55 @@ myObservables.AddObservable(Operators, ['bdagger', 'b'], 'corr', 'spdm')
 
 myConv = mps.MPSConvergenceParameters(max_num_sweeps=7)
 
+L = 6
+Wlist = [2e10, 2e11]
+Nlist = [1, 2, 3]
+
+jobID = 'BH_' + str(int(time.time())) + '_'
+id = 0
+
+seed = int(sys.argv[2])
+delta = float(sys.argv[3])
+np.random.seed(seed);
+xi = 1 + 2 * delta * np.random.random(L) - delta
+
+parameters = []
+for N in Nlist:
+    for W in Wlist:
+        parameters.append({
+            'job_ID': jobID,
+            'unique_ID': str(id),
+            'Write_Directory': 'Temp/',
+            'Output_Directory': 'Output/',
+            'L': L,
+            'J': JW(W*xi),
+            'U': UW(W*xi),
+            'Abelian_generators': ['nbtotal'],  #Working At Unit Filling
+            'Abelian_quantum_numbers': [N],
+            'verbose': 0,
+            'MPSObservables': myObservables,
+            'MPSConvergenceParameters': myConv
+        })
+        id += 1
+
+MainFiles=mps.WriteFiles(parameters,Operators,H,PostProcess=PostProcess)
+
+if not PostProcess:
+    gobject.timeout_add(1000, run, MainFiles)
+    gtk.gdk.threads_init()
+    gtk.main()
+
+quit()
+
 U = 1.0
 tlist = [[t+0.01,t-0.02,t-0.03,t+0.04,t+0.03,t-0.01] for t in np.linspace(0,0.4,20)]
 # tlist = [[t, t, t, t, t, t] for t in np.linspace(0,0.4,20)]
 # tlist = [t for t in np.linspace(0,0.4,20)]
-parameters = []
+tlist = [0,0.01]
+# parameters = []
 L = 6
-Nlist = np.linspace(1, 12, 12)
+Nlist = np.linspace(1, 2*L, 2*L)
+Nlist = [1,2,3]
 
 # count = 0
 
@@ -58,8 +147,8 @@ for N in Nlist:
         parameters.append({
             #Directories
             'job_ID': 'Bose_Hubbard',
-            'unique_ID': 't_' + str(t[0]) + 'N_' + str(N),
-            # 'unique_ID': 't_' + str(t) + 'N_' + str(N),
+            # 'unique_ID': 't_' + str(t[0]) + 'N_' + str(N),
+            'unique_ID': 't_' + str(t) + 'N_' + str(N),
             'Write_Directory': 'Temp/',
             'Output_Directory': 'Output/',
             #System size and Hamiltonian parameters
@@ -83,6 +172,7 @@ MainFiles=mps.WriteFiles(parameters,Operators,H,PostProcess=PostProcess)
 if not PostProcess:
     mps.runMPS(MainFiles,RunDir='./')
 
+
 print time.time() - t0
 
 #Postprocessing and plotting
@@ -95,8 +185,8 @@ if PostProcess:
         depletionlist = []
         Outputs2 = mps.GetObservables(Outputs, 't', [t])
         # Outputs2 = mps.GetObservables(Outputs, 't', t[0])
-        # tinternal = t * np.ones(len(Nlist))
-        tinternal = t[0] * np.ones(len(Nlist))
+        tinternal = t * np.ones(len(Nlist))
+        # tinternal = t[0] * np.ones(len(Nlist))
 
         for Output in Outputs2:
             spdm = Output['spdm']
