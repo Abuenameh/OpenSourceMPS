35d709e6b67c96b2261338471583736af7bef070
Abuenameh Aiyejina <abuenameh@gmail.com>
2014-10-22 23:01:40 -0400

    Commit

--- BoseHubbardStatics.py
+++ BoseHubbardStatics.py
@@ -61,22 +61,6 @@ def JW(W):
 def UW(W):
     return -2*(g24 * g24) / Delta * (Ng * Ng * W * W) / ((Ng * Ng + W * W) * (Ng * Ng + W * W))
 
-# t0 = time.time()
-#
-#
-# def plotIt(jvalues, muvalues, dependentvalues):
-#     plt.rc('text', usetex=True)
-#     plt.rc('font', family='serif')
-#     plt.scatter(jvalues, muvalues, c=dependentvalues, cmap=cm.jet)
-#     plt.xlim((np.min(jvalues), np.max(jvalues)))
-#     plt.ylim((np.min(muvalues), np.max(muvalues)))
-#     plt.xlabel(r"\textbf{tunneling}  " r"$t/U$", fontsize=16)
-#     plt.ylabel(r"\textbf{chemical potential}  " r"$\mu/U$", fontsize=16)
-#     cbar = plt.colorbar()
-#     cbar.set_label(r"\textbf{Quantum Depletion}", fontsize=16)
-#     plt.show()
-
-
 PostProcessOnly = int(sys.argv[1])
 
 if not PostProcessOnly:
@@ -296,87 +280,5 @@ if True:#PostProcess:
 
 quit()
 
-# U = 1.0
-# tlist = [[t+0.01,t-0.02,t-0.03,t+0.04,t+0.03,t-0.01] for t in np.linspace(0,0.4,20)]
-# # tlist = [[t, t, t, t, t, t] for t in np.linspace(0,0.4,20)]
-# # tlist = [t for t in np.linspace(0,0.4,20)]
-# tlist = [0,0.01]
-# # parameters = []
-# L = 6
-# Nlist = np.linspace(1, 2*L, 2*L)
-# Nlist = [1,2,3]
-#
-# # count = 0
-#
-# for N in Nlist:
-#     for t in tlist:
-#         parameters.append({
-#             #Directories
-#             'job_ID': 'Bose_Hubbard',
-#             # 'unique_ID': 't_' + str(t[0]) + 'N_' + str(N),
-#             'unique_ID': 't_' + str(t) + 'N_' + str(N),
-#             'Write_Directory': 'Temp/',
-#             'Output_Directory': 'Output/',
-#             #System size and Hamiltonian parameters
-#             'L': L,
-#             't': t,#[0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01],#t,
-#             'U': U,#[1,1,1,1,1,1],#U,
-#             #Specification of symmetries and good quantum numbers
-#             'Abelian_generators': ['nbtotal'],  #Working At Unit Filling
-#             'Abelian_quantum_numbers': [N],
-#             #Convergence parameters
-#             'verbose': 0,
-#             'MPSObservables': myObservables,
-#             'MPSConvergenceParameters': myConv
-#         })
-#         # count += 1
-#
-#
-# #Write Fortran-readable main files=b
-# MainFiles=mps.WriteFiles(parameters,Operators,H,PostProcess=PostProcess)
-# #Run the simulations if we are not just Post
-# if not PostProcess:
-#     mps.runMPS(MainFiles,RunDir='./')
-
-
-# print time.time() - t0
-#
-# #Postprocessing and plotting
-# if PostProcess:
-#     alldata = [np.array([]), np.array([]), np.array([])]
-#     Outputs = mps.ReadStaticObservables(parameters)
-#     for t in tlist:
-#         mulist = []
-#         energylist = []
-#         depletionlist = []
-#         Outputs2 = mps.GetObservables(Outputs, 't', [t])
-#         # Outputs2 = mps.GetObservables(Outputs, 't', t[0])
-#         tinternal = t * np.ones(len(Nlist))
-#         # tinternal = t[0] * np.ones(len(Nlist))
-#
-#         for Output in Outputs2:
-#             spdm = Output['spdm']
-#             spdmeigs, U = np.linalg.eigh(spdm)
-#             maxeig = np.max(spdmeigs)
-#             eigsum = np.sum(spdmeigs)
-#             depletion = 1 - (maxeig / eigsum)
-#             depletionlist.append(depletion)
-#             energy = Output['energy']
-#             energylist.append(energy)
-#             converged = Output['converged']
-#             print Output['t']
-#
-#             print 'Simulation Converged?:', converged
-#         mulist.append(energylist[0])
-#
-#         for i in range(1, len(energylist), 1):
-#             mu = energylist[i] - energylist[i - 1]
-#             mulist.append(mu)
-#         alldata[0] = np.append(alldata[0], tinternal)
-#         alldata[1] = np.append(alldata[1], mulist)
-#         alldata[2] = np.append(alldata[2], depletionlist)
-#
-#     plotIt(alldata[0], alldata[1], alldata[2])
-#
 
 
